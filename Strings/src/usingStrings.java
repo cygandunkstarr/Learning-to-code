1bc26c8e947c25e88ce2570d52acafc20c033800
cygandunkstarr <dunkstarr@gmail.com>
2018-03-27 10:03:02 +0200

    Using Strings
    Addition, concatenation of strings

---
+++ Strings/src/usingStrings.java
@@ -0,0 +1,18 @@
+public class usingStrings {
+	public static void main(String[] args) {
+		int jerseyNumber = 4;
+
+		String object = "jersey";
+		String owner = "My";
+		String id = "number";
+		String blank = " ";
+		
+		String info = owner + blank + object + blank + id;
+		
+		System.out.println(info + blank + "is: " + jerseyNumber + ".");
+		System.out.println("My jersey number is: 4.");
+		
+		
+	}
+
+}
