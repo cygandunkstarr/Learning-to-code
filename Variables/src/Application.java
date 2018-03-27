6fcfddbbe05fc16c0794558854629107eb8718c6
cygandunkstarr <dunkstarr@gmail.com>
2018-03-27 09:42:29 +0200

    Using Primitive Variables
    Primitive Java Variables

---
+++ Variables/src/Application.java
@@ -0,0 +1,23 @@
+public class Application {
+	public static void main(String[] args) {
+		int myInt = 4;
+		short myShort = 0;
+		long myLong = 144000;
+		double myDouble = 3.1415;
+		float myFloat = 3.1415f;
+		char myChar = 'c';
+		boolean bool = true;
+		byte myByte = 8;
+		// Primitive Variable Types in Java
+		
+		System.out.println(myInt);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(bool);
+		System.out.println(myByte);
+	}
+
+}
