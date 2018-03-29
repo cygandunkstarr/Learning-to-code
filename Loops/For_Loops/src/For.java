8f33e18038bdd0ab172f45ed10285ff38393508a
cygandunkstarr <dunkstarr@gmail.com>
2018-03-29 03:42:20 +0200

    For Loops
    For. Initial condition, conditions for execution, action after each iteration. Printf command, format specifiers and line breaker command.

---
+++ Loops/For_Loops/src/For.java
@@ -0,0 +1,13 @@
+public class For {
+	public static void main(String[] args) {
+		
+		//1. Initial condition 2. Conditions for execution 3. Condition executed once after every iteration
+		for(int myAge = 0; myAge <= 20; myAge++) {
+			System.out.printf("Cyril is %d this year.\n", myAge);
+		}
+		//you can use print f
+		//first argument (%d) is a format specifier and the second (myAge) is a value
+		// \n for new line
+
+	}
+}
