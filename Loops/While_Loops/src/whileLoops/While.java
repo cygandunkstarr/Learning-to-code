2f69704a77c5ed2547b3d97aef3c38356fa95659
cygandunkstarr <dunkstarr@gmail.com>
2018-03-29 03:20:27 +0200

    Loops
    While Loop
    If Loop
    Else if Loop

---
+++ Loops/While_Loops/src/whileLoops/While.java
@@ -0,0 +1,14 @@
+package whileLoops;
+
+public class While {
+	public static void main(String[] args) {
+		int countdown = 10;
+		
+		while(countdown >= 0){
+			System.out.println(countdown + " " + "units to go.");
+			countdown--;		
+			
+		}
+	}
+
+}
