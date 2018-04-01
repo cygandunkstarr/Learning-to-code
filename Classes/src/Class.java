fdb2ba6b051c0e800a84ae36f54e741484a421d0
cygandunkstarr <dunkstarr@gmail.com>
2018-04-01 05:06:55 +0200

    Classes and Methods

--- Classes/src/Class.java
+++ Classes/src/Class.java
@@ -5,8 +5,14 @@ class Person{
 	
 	// Classes can contain
 	
+	//Behavior is modeled using subroutines
+	
 	//1. Data
 	//2. Subroutines (methods)
+	void speak () {
+	//can include a for loop
+		System.out.println("My name is " + name + " and I am " + age + " years old.");
+	}
 }
 
 public class Class {
@@ -16,10 +22,12 @@ public class Class {
 		Person person1 = new Person();
 		person1.name = "Cyril Junior Dim";
 		person1.age = 20;
+		person1.speak();
 		
 		Person person2 = new Person();
 		person2.name = "Donnee Dim";
 		person2.age = 24;
+		person2.speak();
 		
 		Person person3 = new Person();
 		person2.name = "Musa Sibanda";
