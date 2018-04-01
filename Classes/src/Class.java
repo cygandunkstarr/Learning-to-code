4a9aa5c9e373d7ef145d41c184ecfed651e6b0e9
cygandunkstarr <dunkstarr@gmail.com>
2018-04-01 04:42:29 +0200

    Classes
    Classes and adding data to them.

---
+++ Classes/src/Class.java
@@ -0,0 +1,31 @@
+class Person{
+	//Instance variables (data or "state")
+	String name;
+	int age;
+	
+	// Classes can contain
+	
+	//1. Data
+	//2. Subroutines (methods)
+}
+
+public class Class {
+
+	public static void main(String[] args) {
+		
+		Person person1 = new Person();
+		person1.name = "Cyril Junior Dim";
+		person1.age = 20;
+		
+		Person person2 = new Person();
+		person2.name = "Donnee Dim";
+		person2.age = 24;
+		
+		Person person3 = new Person();
+		person2.name = "Musa Sibanda";
+		person2.age = 50;
+				
+
+	}
+
+}
